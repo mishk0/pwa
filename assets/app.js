a03c5c1273f5a2b223c5f8c966426cb39208493b
mishk0 <mokrushin.ms@yandex.ru>
2017-07-08 21:00:12 +0300

    init

--- assets/app.js
+++ assets/app.js
@@ -1,9 +1,60 @@
 (function() {
     'use strict';
+    var API_URL = 'http://api.fixer.io/';
+    var AUTO_UPDATE_SEC = 5;
+    var currenciesNode = document.querySelector('.currencies');
+    var currencyRates = {};
+
+    function init() {
+        updateCurrency();
+
+        setInterval(() => {
+            updateCurrency();
+        }, AUTO_UPDATE_SEC * 1000);
+
+        bindEvents();
+    }
+
+    function bindEvents() {
+        document.querySelector('.refresh').addEventListener('click', updateCurrency);
+    }
+
+    function updateCurrency() {
+        return getCurrency().then(rates => render(rates));
+    }
+
+    function getCurrency() {
+        return fetch(API_URL + '/latest')
+            .then(res => res.json())
+            .then(data => currencyRates = data.rates);
+    }
+
+    function render(rates) {
+        currenciesNode.innerHTML = createTmpl(rates);
+    }
+
+    function createTmpl(data) {
+        return Object.keys(data).reduce((res, item) => {
+            var additionClass = '';
+
+            if (data[item] > currencyRates[item]) {
+                additionClass = 'currency-rate_up';
+            } else if (data[item] < currencyRates[item]) {
+                additionClass = 'currency-rate_down';
+            }
+
+            return res + `<div class="currency">
+                <span class="currency-name">${item}</span>
+                <span class="currency-rate ${additionClass}">${data[item]}</span>
+            </div>`;
+        }, '');
+    }
 
     if ('serviceWorker' in navigator) {
         navigator.serviceWorker
             .register('./service-worker.js')
             .then(() => { console.log('Service Worker Registered') });
     }
+
+    init();
 })();
