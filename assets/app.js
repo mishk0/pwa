70d87d9d579185d911f04d8620b4e8d9efae8aa1
mishk0 <mokrushin.ms@yandex.ru>
2017-07-08 20:05:02 +0300

    init

---
+++ assets/app.js
@@ -0,0 +1,9 @@
+(function() {
+    'use strict';
+
+    if ('serviceWorker' in navigator) {
+        navigator.serviceWorker
+            .register('./service-worker.js')
+            .then(() => { console.log('Service Worker Registered') });
+    }
+})();
---
+++ service-worker.js
@@ -0,0 +1,8 @@
+'use strict';
+
+const CACHE_NAME = 'v1';
+
+self.addEventListener('install', () => {});
+self.addEventListener('activate', () => {});
+self.addEventListener('fetch', () => {});
+
