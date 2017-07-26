61c0260ea1279b64165636df00cb3384b9c317a2
mishk0 <mokrushin.ms@yandex.ru>
2017-07-26 10:30:40 +0300

    stage2

--- service-worker.js
+++ service-worker.js
@@ -34,50 +34,18 @@ self.addEventListener('fetch', function(e) {
     }
 });
 
-// v1
-//function networkFirst(req) {
-//    return caches.open(CACHE_NAME).then(function(cache) {
-//        return fetch(req).then(function(res){
-//            cache.put(req, res.clone());
-//            return res;
-//        })
-//    })
-//}
-
-// v1
-//function cacheFirst(req) {
-//    return caches.match(req).then(function(cache) {
-//        return cache || fetch(req);
-//    })
-//}
-
-//v2
 function networkFirst(req) {
     return caches.open(CACHE_NAME).then(function(cache) {
         return fetch(req).then(function(res){
             cache.put(req, res.clone());
             return res;
-        }).catch(err => {
-            console.log('Error on networkFirst', err);
-
-            return caches.match(req);
-        });
+        })
     })
 }
 
-//v2
 function cacheFirst(req) {
     return caches.match(req).then(function(cache) {
-        if (cache) {
-            return cache;
-        }
-
-        return caches.open(CACHE_NAME).then(cache => {
-            return fetch(req).then(function(res) {
-                cache.put(req, res.clone());
-                return res;
-            });
-        });
+        return cache || fetch(req);
     })
 }
 
@@ -85,7 +53,6 @@ function isApiCall(url) {
     return url.indexOf(API_URL) !== -1;
 }
 
-//v1
 function deleteObsoleteAssets() {
     return caches.keys().then(function(keys) {
         return Promise.all(keys.map(function(key) {
@@ -95,37 +62,3 @@ function deleteObsoleteAssets() {
         }));
     })
 }
-
-// v2
-//function cacheNewAssets(cacheName, assets) {
-//    return caches.open(cacheName).then(cache => {
-//        return cache.keys().then(requests => {
-//            var cachedAssets = requests.map(getRequestUrl);
-//            var newAssets = assets.filter(path => cachedAssets.indexOf(path) === -1);
-//
-//            return cache.addAll(newAssets);
-//        })
-//    })
-//}
-
-// v2
-//function deleteObsoleteAssets(cacheName, assets) {
-//    return Promise.all([
-//        caches.keys()
-//            .then(names => {
-//                return Promise.all(
-//                    names.filter(name => name !== cacheName)
-//                        .map(name => caches.delete(name))
-//                );
-//            }),
-//        caches.open(cacheName).then(cache => {
-//            return cache.keys().then(requests => {
-//                var cachedAssets = requests.map(getRequestUrl);
-//                var oldAssets = cachedAssets.filter(path => assets.indexOf(path) === -1);
-//
-//                return Promise.all(oldAssets.map(asset => cache.delete(asset)));
-//            })
-//        })
-//    ])
-//}
-
