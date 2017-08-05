537db47841d0542c3897de081deff4215a116f0f
Maria Evstropova <m.evstropova@yandex.ru>
2017-08-05 20:03:41 +0300

    fetch with timeout, smart add and delete cache

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const CACHE_NAME = 'v1';
+const SEC_TIMEOUT = 5;
 var API_URL = 'https://api.fixer.io';
 
 var filesToCache = [
@@ -13,21 +14,19 @@ var filesToCache = [
 self.addEventListener('install', function(e) {
     e.waitUntil(
         self.skipWaiting(),
-        caches.open(CACHE_NAME).then(function(cache) {
-            return cache.addAll(filesToCache);
-        })
+        addNewAssets(filesToCache)
     );
 });
 
 self.addEventListener('activate', function(e) {
     self.clients.claim();
 
-    e.waitUntil(deleteObsoleteAssets());
+    e.waitUntil(deleteObsoleteAssets(filesToCache));
 });
 
 self.addEventListener('fetch', function(e) {
     if (isApiCall(e.request.url)) {
-        e.respondWith(networkFirst(e.request));
+        e.respondWith(networkFirstWithTimeout(e.request, SEC_TIMEOUT * 1000));
     } else {
         e.respondWith(cacheFirst(e.request));
     }
@@ -44,6 +43,29 @@ function networkFirst(req) {
             return caches.match(req);
         });
     })
+}
+
+function networkFirstWithTimeout(req, timeout) {
+    let fetchRpomise = caches.open(CACHE_NAME).then(function(cache) {
+        return fetch(req).then(function(res){
+            cache.put(req, res.clone());
+            return res;
+        });
+    });
+
+    let fallBackPromise = new Promise(function(resolve, reject) {
+        return caches.match(req).then(function(cache) {
+            setTimeout(function() {
+                if (cache) {
+                    resolve(cache);
+                }
+            }, timeout);
+        })
+    });
+
+    return Promise.race([fetchRpomise, fallBackPromise]).then(function(res) {
+        return res;
+    });
 }
 
 function cacheFirst(req) {
@@ -65,12 +87,57 @@ function isApiCall(url) {
     return url.indexOf(API_URL) !== -1;
 }
 
-function deleteObsoleteAssets() {
+function addNewAssets(assets) {
+    return caches.open(CACHE_NAME).then(function(cache) {
+        return Promise.all(
+            assets.map((asset) => {
+                return cache.match(asset).then((matched) => {
+                    if (!matched) {
+                        return cache.add(asset);
+                    }
+                });
+            })
+        );
+    });
+}
+
+function deleteObsoleteAssets(assets) {
+    const origin = location.origin;
+    const absoluteAssets = assets.map((asset) => {
+        return asset.replace(/^\./, origin);
+    });
+    const obsoleteAssets = [];
+
     return caches.keys().then(function(keys) {
         return Promise.all(keys.map(function(key) {
             if (key !== CACHE_NAME) {
                 return caches.delete(key);
             }
         }));
+    })
+    .then(() => {
+        return caches.open(CACHE_NAME).then(function(cache) {
+            return cache.keys().then((keys) => {
+                keys.forEach((key) => {
+                    for (let i = 0; i < absoluteAssets.length; i++) {
+                        if (key.url === absoluteAssets[i]) {
+                            return;
+                        }
+
+                        if ( i === absoluteAssets.length - 1) {
+                            obsoleteAssets.push(
+                                cache.delete(key)
+                            );
+                        }
+                    }
+                });
+            })
+            .then(() => {
+                return Promise.all(obsoleteAssets);
+            })
+            .catch((error) => {
+                console.error('error while deleteObsoleteAssets', error)
+            })
+        });
     })
 }
