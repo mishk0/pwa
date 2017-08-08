a705d89807cb5fb4b3b84ae3c4dad11bb9a0110c
Dmitriy Batogov <hi@batogov.ru>
2017-08-08 11:04:06 +0300

    Обновил service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,20 +1,23 @@
 'use strict';
 
 const CACHE_NAME = 'v1';
+const STATIC_NAME = 'static';
+const NETWORK_FIRST_TIMEOUT = 5;
 var API_URL = 'https://api.fixer.io';
 
-var filesToCache = [
-    './',
+var staticToCache = [
     './index.html',
     './assets/app.js',
-    './assets/styles.css'
+    './assets/styles.css',
+    './assets/refresh.svg',
+    './assets/icons/icon152.png'
 ];
 
 self.addEventListener('install', function(e) {
     e.waitUntil(
         self.skipWaiting(),
-        caches.open(CACHE_NAME).then(function(cache) {
-            return cache.addAll(filesToCache);
+        caches.open(STATIC_NAME).then(function(cache) {
+            return cache.addAll(staticToCache);
         })
     );
 });
@@ -22,7 +25,11 @@ self.addEventListener('install', function(e) {
 self.addEventListener('activate', function(e) {
     self.clients.claim();
 
-    e.waitUntil(deleteObsoleteAssets());
+    e.waitUntil(
+        addNewAssets(),
+        deleteObsoleteAssets(),
+        deleteObsoleteCaches()
+    );
 });
 
 self.addEventListener('fetch', function(e) {
@@ -33,26 +40,63 @@ self.addEventListener('fetch', function(e) {
     }
 });
 
-function networkFirst(req) {
-    return caches.open(CACHE_NAME).then(function(cache) {
-        return fetch(req).then(function(res){
-            cache.put(req, res.clone());
-            return res;
-        }).catch(err => {
-            console.log('Error on networkFirst', err);
+self.addEventListener('sync', function(event) {
+    if (event.tag === 'force-update-push') {
+        event.waitUntil(updateCurrency().then(() => {
+            self.registration.showNotification('Currency', {
+                body: 'Currencies successfully updated',
+                icon: './assets/icons/icon152.png'
+            });
+        }));
+    } else if (event.tag === 'force-update') {
+        event.waitUntil(updateCurrency());
+    }
+});
 
-            return caches.match(req);
-        });
-    })
+/**
+ * Функция используется для обработки запросов к API. Сначала она
+ * отправляет запрос в сеть, а по истечении таумаута (константа 
+ * NETWORK_FIRST_TIMEOUT) берёт данные из кеша.
+ */
+function networkFirst(req) {
+    var myHeaders = new Headers();
+    myHeaders.append('pragma', 'no-cache');
+    myHeaders.append('cache-control', 'no-cache');
+    var myInit = {
+        method: 'GET',
+        headers: myHeaders,
+    };
+
+    const race = Promise.race([
+        new Promise((resolve, reject) => {
+            let wait = setTimeout(() => {
+                clearTimeout(wait);
+                reject();
+            }, NETWORK_FIRST_TIMEOUT * 1000);
+        }),
+        fetch(req, myInit),
+    ]);
+
+    return race.then((res) => {
+        return caches.open(CACHE_NAME).then(cache => {
+            cache.put(req, res.clone());
+        }).then(() => res);
+    }, error => {
+        return caches.match(req);
+    });
 }
 
+/**
+ * Функция для работы с запросами на загрузку статики. Сначала пытается найти
+ * статику в кеше, а в случае неудачи идёт в сеть.
+ */
 function cacheFirst(req) {
     return caches.match(req).then(function(cache) {
         if (cache) {
             return cache;
         }
 
-        return caches.open(CACHE_NAME).then(cache => {
+        return caches.open(STATIC_NAME).then(cache => {
             return fetch(req).then(function(res) {
                 cache.put(req, res.clone());
                 return res;
@@ -61,16 +105,64 @@ function cacheFirst(req) {
     })
 }
 
+/**
+ * Функция возвращает true, если ей был передан URL запроса к API.
+ */
 function isApiCall(url) {
     return url.indexOf(API_URL) !== -1;
 }
 
+/**
+ * Функция удаляет неактуальные файлы из кеша статики.
+ */
 function deleteObsoleteAssets() {
+    return caches.open(STATIC_NAME).then(cache => {
+        return cache.keys().then(keys => {
+            // Идём по всем закешированным файлам в кеше
+            return Promise.all(keys.map(function(key) {
+                // Имя текущего файла
+                const filename = './' + key.url.split('/').slice(3).join('/');
+                // Удаляем файл из кеша, если его больше нет в массиве staticToCache
+                if (staticToCache.indexOf(filename) === -1) {
+                    return cache.delete(key);
+                }
+            }))
+        });
+    });
+}
+
+/**
+ * Функция добавляет новые файлы в кеш статики.
+ */
+function addNewAssets() {
+    return caches.open(STATIC_NAME).then(cache => {
+        staticToCache.forEach(file => {
+            cache.match(file).then((res) => {
+                if (!res) {
+                    return cache.add(file);
+                }
+            });
+        });
+    });
+}
+
+/**
+ * Функция удаляет кеши старых версий.
+ */
+function deleteObsoleteCaches() {
     return caches.keys().then(function(keys) {
         return Promise.all(keys.map(function(key) {
-            if (key !== CACHE_NAME) {
+            if (key !== CACHE_NAME && key !== STATIC_NAME) {
                 return caches.delete(key);
             }
         }));
     })
+}
+
+function updateCurrency() {
+    return fetch(API_URL + '/latest?base=RUB').then(res => {
+        return caches.open(CACHE_NAME).then(cache => {
+            return cache.put(res.url, res);
+        })
+    });
 }
